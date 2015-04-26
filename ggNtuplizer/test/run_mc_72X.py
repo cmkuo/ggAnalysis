6b5d3dd235bf77650dfac6f7ae4e2d4e6978ce9e
Shilpi <shilpi.jain@cern.ch>
2015-04-26 18:22:26 +0200

    Added information about photons and electrons. --Tags to be checked out: (1) For photons: git cms-merge-topic ikrav:egm_id_phys14 (2) For elecrons: git cms-merge-topic HuguesBrun:trigElecIdInCommonIsoSelection720 -------

---
+++ ggNtuplizer/test/run_mc_72X.py
@@ -0,0 +1,143 @@
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+## switch to uncheduled mode
+process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.Tracer = cms.Service('Tracer')
+
+#Lvdp
+#process.load("PhysicsTools.PatAlgos.producersLayer1.jetProducer_cff")
+#process.load("PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi")
+#process.load('PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff')
+
+
+
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+from PhysicsTools.PatAlgos.tools.jetTools import switchJetCollection
+
+addJetCollection(
+    process,
+    labelName = 'AK4PFCHS',
+    jetSource = cms.InputTag('ak4PFJetsCHS'),
+    algo = 'ak',
+    rParam = 0.4,
+    jetCorrections = ('AK5PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+    btagDiscriminators = [
+        'jetBProbabilityBJetTags'
+      , 'jetProbabilityBJetTags'
+      , 'trackCountingHighPurBJetTags'
+      , 'trackCountingHighEffBJetTags'
+      , 'simpleSecondaryVertexHighEffBJetTags'
+      , 'simpleSecondaryVertexHighPurBJetTags'
+      , 'combinedSecondaryVertexBJetTags'
+      ],
+)
+addJetCollection(
+    process,
+    labelName = 'CA8PFCHS',
+    jetSource = cms.InputTag('ca8PFJetsCHS'),
+    algo = 'ca',
+    rParam = 0.8,
+    jetCorrections = ('AK7PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None')
+    )
+
+addJetCollection(
+    process,
+    labelName = 'AK8PFCHS',
+    jetSource = cms.InputTag('ak8PFJetsCHS'),
+    algo = 'ak',
+    rParam = 0.8,
+    jetCorrections = ('AK7PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None')
+    )
+process.ca8PFJetsCHSPrunedLinks = cms.EDProducer("RecoJetDeltaRValueMapProducer",
+                                         src = cms.InputTag("ca8PFJetsCHS"),
+                                         matched = cms.InputTag("ca8PFJetsCHSPruned"),
+                                         distMax = cms.double(0.8),
+                                         value = cms.string('mass')
+                        )
+
+
+patJetsAK4 = process.patJetsAK4PFCHS
+patJetsCA8 = process.patJetsCA8PFCHS
+patJetsAK8 = process.patJetsAK8PFCHS
+
+process.out.outputCommands += ['keep *_selectedPat*_*_*',
+                                'keep *_ak4PFJetsCHS_*_*',
+                               'keep *_patJetsAK4PFCHS_*_*',
+                               'keep *_ca8PFJetsCHS_*_*',
+                               'keep *_patJetsCA8PFCHS_*_*',
+                               'keep *_ak8PFJetsCHS_*_*',
+                               'keep *_patJetsAK8PFCHS_*_*',
+				'keep *_*ca8PFJetsCHSPruned*_*_*'
+]
+patJetsCA8.userData.userFloats.src += ['ca8PFJetsCHSPrunedLinks']
+process.out.outputCommands += ['keep *_ca8PFJetsCHSPrunedLinks_*_*']
+
+
+#### Adding Nsubjetiness
+
+process.NjettinessCA8 = cms.EDProducer("NjettinessAdder",
+                              src=cms.InputTag("ca8PFJetsCHS"),
+                              cone=cms.double(0.8),
+                              Njets=cms.vuint32(1,2,3)
+                              )
+
+patJetsCA8.userData.userFloats.src += ['NjettinessCA8:tau1','NjettinessCA8:tau2','NjettinessCA8:tau3']
+process.out.outputCommands += ['keep *_NjettinessCA8_*_*']
+
+
+process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_cfi")
+
+##Taus
+from PhysicsTools.PatAlgos.tools.tauTools import *
+process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
+process.cleanPatTaus.preselection = cms.string(' tauID("decayModeFinding") > 0.5 ')
+process.cleanPatTaus.finalCut = cms.string(' pt > 15.0 & abs(eta) < 2.5 ')
+process.load("ggAnalysis.ggNtuplizer.ggTau_cff")
+##Jets
+
+process.ggNtuplizer.dumpSubJets=cms.bool(False)
+process.ggNtuplizer.dumpJets=cms.bool(False)
+process.ggNtuplizer.dumpTaus=cms.bool(False)
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_mc.root'))
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+        #'file:/tmp/cmkuo/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6_Phys14.root'
+        #'file:/data4/cmkuo/testfiles/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6_Phys14.root'
+        'root://cmsxrootd.fnal.gov///store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00CC714A-F86B-E411-B99A-0025904B5FB8.root'
+        )
+                            )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+#process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+
+###9th April, 2015 SHILPI JAIN
+###ADDING ELECTRON ID MVA IN THE PATH: https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
+
+process.load("EgammaAnalysis.ElectronTools.electronIdMVAProducer_CSA14_cfi")
+
+setattr(process.patElectrons.electronIDSources, "trigMVAid",cms.InputTag("mvaTrigV050nsCSA14"))
+
+process.p = cms.Path(
+#process.egmGsfElectronIDSequence
+ process.mvaTrigV050nsCSA14
+ + process.mvaTrigV025nsCSA14
+ + process.mvaNonTrigV050nsCSA14
+ + process.mvaNonTrigV025nsCSA14
+ + process.mvaNonTrigV025nsPHYS14 
+ + process.patDefaultSequence
+ * process.NjettinessCA8
+ * process.ggNtuplizer
+)
+
+#process.p = cms.Path(
+#process.patDefaultSequence
+# *    process.NjettinessCA8
+# *  process.ggNtuplizer
+#)
+
+process.outpath = cms.EndPath(process.out)
+process.outpath.remove(process.out)
+
