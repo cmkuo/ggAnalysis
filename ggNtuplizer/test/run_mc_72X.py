dbdf88457e15d218b60fa539ddee2d69a2b2fc13
Shilpi <shilpi.jain@cern.ch>
2015-05-19 09:40:37 +0200

    Fixed MC to run on AOD

--- ggNtuplizer/test/run_mc_72X.py
+++ ggNtuplizer/test/run_mc_72X.py
@@ -1,54 +1,17 @@
-from PhysicsTools.PatAlgos.patTemplate_cfg import *
-## switch to uncheduled mode
-process.options.allowUnscheduled = cms.untracked.bool(True)
-#process.Tracer = cms.Service('Tracer')
+import FWCore.ParameterSet.Config as cms
 
-#Lvdp
-#process.load("PhysicsTools.PatAlgos.producersLayer1.jetProducer_cff")
-#process.load("PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi")
-#process.load('PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff')
+process = cms.Process('ggNtuplizer')
 
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.load("Configuration.StandardSequences.MagneticField_cff")
 
-
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
-from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
-from PhysicsTools.PatAlgos.tools.jetTools import switchJetCollection
-
-addJetCollection(
-    process,
-    labelName = 'AK4PFCHS',
-    jetSource = cms.InputTag('ak4PFJetsCHS'),
-    algo = 'ak',
-    rParam = 0.4,
-    jetCorrections = ('AK5PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
-    btagDiscriminators = [
-        'jetBProbabilityBJetTags'
-      , 'jetProbabilityBJetTags'
-      , 'trackCountingHighPurBJetTags'
-      , 'trackCountingHighEffBJetTags'
-      , 'simpleSecondaryVertexHighEffBJetTags'
-      , 'simpleSecondaryVertexHighPurBJetTags'
-      , 'combinedSecondaryVertexBJetTags'
-      ],
+process.out = cms.OutputModule("PoolOutputModule",
+                               fileName = cms.untracked.string('outTuple.root'),
 )
-addJetCollection(
-    process,
-    labelName = 'CA8PFCHS',
-    jetSource = cms.InputTag('ca8PFJetsCHS'),
-    algo = 'ca',
-    rParam = 0.8,
-    jetCorrections = ('AK7PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None')
-    )
 
-addJetCollection(
-    process,
-    labelName = 'AK8PFCHS',
-    jetSource = cms.InputTag('ak8PFJetsCHS'),
-    algo = 'ak',
-    rParam = 0.8,
-    jetCorrections = ('AK7PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None')
-    )
 process.ca8PFJetsCHSPrunedLinks = cms.EDProducer("RecoJetDeltaRValueMapProducer",
                                          src = cms.InputTag("ca8PFJetsCHS"),
                                          matched = cms.InputTag("ca8PFJetsCHSPruned"),
@@ -57,42 +20,22 @@ process.ca8PFJetsCHSPrunedLinks = cms.EDProducer("RecoJetDeltaRValueMapProducer"
                         )
 
 
-patJetsAK4 = process.patJetsAK4PFCHS
-patJetsCA8 = process.patJetsCA8PFCHS
-patJetsAK8 = process.patJetsAK8PFCHS
-
-process.out.outputCommands += ['keep *_selectedPat*_*_*',
-                                'keep *_ak4PFJetsCHS_*_*',
-                               'keep *_patJetsAK4PFCHS_*_*',
-                               'keep *_ca8PFJetsCHS_*_*',
-                               'keep *_patJetsCA8PFCHS_*_*',
-                               'keep *_ak8PFJetsCHS_*_*',
-                               'keep *_patJetsAK8PFCHS_*_*',
-				'keep *_*ca8PFJetsCHSPruned*_*_*'
-]
-patJetsCA8.userData.userFloats.src += ['ca8PFJetsCHSPrunedLinks']
-process.out.outputCommands += ['keep *_ca8PFJetsCHSPrunedLinks_*_*']
-
-
-#### Adding Nsubjetiness
-
 process.NjettinessCA8 = cms.EDProducer("NjettinessAdder",
                               src=cms.InputTag("ca8PFJetsCHS"),
                               cone=cms.double(0.8),
                               Njets=cms.vuint32(1,2,3)
                               )
 
-patJetsCA8.userData.userFloats.src += ['NjettinessCA8:tau1','NjettinessCA8:tau2','NjettinessCA8:tau3']
-process.out.outputCommands += ['keep *_NjettinessCA8_*_*']
+#process.out.outputCommands += ['keep *_NjettinessCA8_*_*'] // FIXME: uncomment?
 
 
 process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_cfi")
 
 ##Taus
-from PhysicsTools.PatAlgos.tools.tauTools import *
+#from PhysicsTools.PatAlgos.tools.tauTools import *
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
-process.cleanPatTaus.preselection = cms.string(' tauID("decayModeFinding") > 0.5 ')
-process.cleanPatTaus.finalCut = cms.string(' pt > 15.0 & abs(eta) < 2.5 ')
+#process.cleanPatTaus.preselection = cms.string(' tauID("decayModeFinding") > 0.5 ')
+#process.cleanPatTaus.finalCut = cms.string(' pt > 15.0 & abs(eta) < 2.5 ')
 process.load("ggAnalysis.ggNtuplizer.ggTau_cff")
 ##Jets
 
@@ -105,8 +48,8 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
         #'file:/tmp/cmkuo/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6_Phys14.root'
-        #'file:/data4/cmkuo/testfiles/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6_Phys14.root'
-        'root://cmsxrootd.fnal.gov///store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00CC714A-F86B-E411-B99A-0025904B5FB8.root'
+        'file:/data4/cmkuo/testfiles/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6_Phys14.root'
+        #'root://cmsxrootd.fnal.gov///store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00CC714A-F86B-E411-B99A-0025904B5FB8.root'
         )
                             )
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
@@ -116,20 +59,54 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 ###9th April, 2015 SHILPI JAIN
 ###ADDING ELECTRON ID MVA IN THE PATH: https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
 
-process.load("EgammaAnalysis.ElectronTools.electronIdMVAProducer_CSA14_cfi")
+#process.load("EgammaAnalysis.ElectronTools.electronIdMVAProducer_CSA14_cfi")
 
-setattr(process.patElectrons.electronIDSources, "trigMVAid",cms.InputTag("mvaTrigV050nsCSA14"))
+#setattr(process.patElectrons.electronIDSources, "trigMVAid",cms.InputTag("mvaTrigV050nsCSA14"))
+
+
+useAOD = True
+#####VID framework####################
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format  to be
+# DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
+if useAOD == True :
+    dataFormat = DataFormat.AOD
+else :
+    dataFormat = DataFormat.MiniAOD
+
+process.ggNtuplizer.isAOD=cms.bool(useAOD)
+
+switchOnVIDElectronIdProducer(process, dataFormat)
+switchOnVIDPhotonIdProducer(process, dataFormat)
+
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V2_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV51_cff']
+
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+    
+my_phoid_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_PHYS14_PU20bx25_V2_cff']
+
+#add them to the VID producer
+for idmod in my_phoid_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
+
+
 
 process.p = cms.Path(
 #process.egmGsfElectronIDSequence
- process.mvaTrigV050nsCSA14
- + process.mvaTrigV025nsCSA14
- + process.mvaNonTrigV050nsCSA14
- + process.mvaNonTrigV025nsCSA14
- + process.mvaNonTrigV025nsPHYS14 
- + process.patDefaultSequence
- * process.NjettinessCA8
- * process.ggNtuplizer
+ #process.mvaTrigV050nsCSA14
+ #+ process.mvaTrigV025nsCSA14
+ #+ process.mvaNonTrigV050nsCSA14
+ #+ process.mvaNonTrigV025nsCSA14
+ #+ process.mvaNonTrigV025nsPHYS14
+ #+ process.patDefaultSequence
+ #* process.NjettinessCA8
+    process.egmGsfElectronIDSequence 
+    * process.egmPhotonIDSequence 
+    * process.ggNtuplizer
 )
 
 #process.p = cms.Path(
