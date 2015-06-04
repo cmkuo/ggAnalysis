3a07db0907aebea564d1d8601f930d71c9ce1bcb
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2015-06-04 06:57:20 +0200

    add Run2 HLT triggers

--- ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
+++ ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
@@ -14,14 +14,13 @@ Int_t     nVtx_;
 Int_t     nTrksPV_;
 float     rho_;
 ULong64_t HLT_;
-
-float    genMET_;
-float    genMETPhi_;
-float    pfMET_;
-float    pfMETPhi_;
-float    pfMETsumEt_;
-float    pfMETmEtSig_;
-float    pfMETSig_;
+float     genMET_;
+float     genMETPhi_;
+float     pfMET_;
+float     pfMETPhi_;
+float     pfMETsumEt_;
+float     pfMETmEtSig_;
+float     pfMETSig_;
 
 void ggNtuplizer::branchesGlobalEvent(TTree* tree) {
 
@@ -90,59 +89,20 @@ void ggNtuplizer::fillGlobalEvent(const edm::Event& e, const edm::EventSetup& es
     ULong64_t bit = (trgResultsHandle->accept(i) && hltCfg.prescaleValue(e, es, name) != 0) ? 1 : 0;
 
     // HLT name => bit correspondence
-    if      (!name.compare("HLT_Photon26_R9Id85_OR_CaloId10_Iso50_Photon18_R9Id85_OR_CaloId10_Iso50_Mass60_v")      ) HLT_ |= (bit<< 0);  // bit 0 (lowest)
-    else if (!name.compare("HLT_Photon26_Photon18_v")                                                               ) HLT_ |= (bit<< 1);  // bit 1
-    else if (!name.compare("HLT_Photon26_CaloId10_Iso50_Photon18_CaloId10_Iso50_Mass60_v")                          ) HLT_ |= (bit<< 2);  // ...
-    else if (!name.compare("HLT_Photon26_CaloId10_Iso50_Photon18_R9Id85_Mass60_v")                                  ) HLT_ |= (bit<< 3);
-    else if (!name.compare("HLT_Photon26_R9Id85_OR_CaloId10_Iso50_Photon18_v")                                      ) HLT_ |= (bit<< 4);
-    else if (!name.compare("HLT_Photon26_R9Id85_Photon18_CaloId10_Iso50_Mass60_v")                                  ) HLT_ |= (bit<< 5);
-    else if (!name.compare("HLT_Photon26_R9Id85_Photon18_R9Id85_Mass60_v")                                          ) HLT_ |= (bit<< 6);
-    else if (!name.compare("HLT_Photon26_R9Id85_OR_CaloId10_Iso50_Photon18_R9Id85_OR_CaloId10_Iso50_Mass70_v")      ) HLT_ |= (bit<< 7);
-    else if (!name.compare("HLT_Photon36_R9Id85_OR_CaloId10_Iso50_Photon22_R9Id85_OR_CaloId10_Iso50_v")             ) HLT_ |= (bit<< 8);
-    else if (!name.compare("HLT_Photon36_R9Id85_OR_CaloId10_Iso50_Photon10_R9Id85_OR_CaloId10_Iso50_Mass80_v")      ) HLT_ |= (bit<< 9);
-    else if (!name.compare("HLT_Photon36_Photon22_v")                                                               ) HLT_ |= (bit<<10);
-    else if (!name.compare("HLT_PAPhoton15_TightCaloIdVL_v")                                                        ) HLT_ |= (bit<<11);
-    else if (!name.compare("HLT_Photon30_R9Id90_CaloId_HE10_Iso40_EBOnly_v")                                        ) HLT_ |= (bit<<12);
-    else if (!name.compare("HLT_Photon30_R9Id90_CaloId_HE10_Iso40_EBOnly_Met25_HBHENoiseCleaned_v")                 ) HLT_ |= (bit<<13);
-    else if (!name.compare("HLT_Photon30_v")                                                                        ) HLT_ |= (bit<<14);
-    else if (!name.compare("HLT_Photon30_CaloIdVL_v")                                                               ) HLT_ |= (bit<<15);
-    else if (!name.compare("HLT_Photon50_CaloIdVL_IsoL_v")                                                          ) HLT_ |= (bit<<16);
-    else if (!name.compare("HLT_Photon50_CaloIdVL_v")                                                               ) HLT_ |= (bit<<17);
-    else if (!name.compare("HLT_Photon75_CaloIdVL_v")                                                               ) HLT_ |= (bit<<18);
-    else if (!name.compare("HLT_Photon90_CaloIdVL_v")                                                               ) HLT_ |= (bit<<19);
-    else if (!name.compare("HLT_Photon135_v")                                                                       ) HLT_ |= (bit<<20);
-    else if (!name.compare("HLT_Photon150_v")                                                                       ) HLT_ |= (bit<<21);
-    else if (!name.compare("HLT_Photon160_v")                                                                       ) HLT_ |= (bit<<22);
-    else if (!name.compare("HLT_Photon36_CaloId10_Iso50_Photon22_CaloId10_Iso50_v")                                 ) HLT_ |= (bit<<23);
-    else if (!name.compare("HLT_Photon36_CaloId10_Iso50_Photon22_R9Id85_v")                                         ) HLT_ |= (bit<<24);
-    else if (!name.compare("HLT_Photon36_R9Id85_Photon22_CaloId10_Iso50_v")                                         ) HLT_ |= (bit<<25);
-    else if (!name.compare("HLT_Photon36_R9Id85_Photon22_R9Id85_v")                                                 ) HLT_ |= (bit<<26);
-    else if (!name.compare("HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v")) HLT_ |= (bit<<27);
-    else if (!name.compare("HLT_Ele27_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele15_CaloIdT_CaloIsoVL_trackless_v")      ) HLT_ |= (bit<<28);
-    else if (!name.compare("HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v")                               ) HLT_ |= (bit<<29);
-    else if (!name.compare("HLT_Ele27_WP80_v")                                                                      ) HLT_ |= (bit<<30);
-    else if (!name.compare("HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v")) HLT_ |= (bit<<31);
-    else if (!name.compare("HLT_Ele20_CaloIdVT_CaloIsoVT_TrkIdT_TrkIsoVT_SC4_Mass50_v")                             ) HLT_ |= (bit<<32);
-    else if (!name.compare("HLT_Mu22_TkMu22_v")                                                                     ) HLT_ |= (bit<<33);
-    else if (!name.compare("HLT_Mu17_TkMu8_v")                                                                      ) HLT_ |= (bit<<34);
-    else if (!name.compare("HLT_Mu17_Mu8_v")                                                                        ) HLT_ |= (bit<<35);
-    else if (!name.compare("HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v")                                    ) HLT_ |= (bit<<36);
-    else if (!name.compare("HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v")                                    ) HLT_ |= (bit<<37);
-    else if (!name.compare("HLT_Mu22_TkMu8_v")                                                                      ) HLT_ |= (bit<<38);
-    else if (!name.compare("HLT_Mu22_Mu8_v")                                                                        ) HLT_ |= (bit<<39);
-    else if (!name.compare("HLT_Mu22_Photon22_CaloIdL_v")                                                           ) HLT_ |= (bit<<40);
-    else if (!name.compare("HLT_IsoMu24_eta2p1_v")                                                                  ) HLT_ |= (bit<<41);
-    else if (!name.compare("HLT_IsoMu24_v")                                                                         ) HLT_ |= (bit<<42);
-    else if (!name.compare("HLT_PAMu12_v")                                                                          ) HLT_ |= (bit<<43);
-    else if (!name.compare("HLT_PAL1DoubleMuOpen_v")                                                                ) HLT_ |= (bit<<44);
-    else if (!name.compare("HLT_DiJet35_MJJ650_AllJets_DEta3p5_VBF_v")                                              ) HLT_ |= (bit<<45);
-    else if (!name.compare("HLT_DiJet35_MJJ700_AllJets_DEta3p5_VBF_v")                                              ) HLT_ |= (bit<<46);
-    else if (!name.compare("HLT_PFJet40_v")                                                                         ) HLT_ |= (bit<<47);
-    else if (!name.compare("HLT_PFJet80_v")                                                                         ) HLT_ |= (bit<<48);
-    else if (!name.compare("HLT_PFJet140_v")                                                                        ) HLT_ |= (bit<<49);
-    else if (!name.compare("HLT_PFJet200_v")                                                                        ) HLT_ |= (bit<<50);
-    else if (!name.compare("HLT_PFJet260_v")                                                                        ) HLT_ |= (bit<<51);
-    else if (!name.compare("HLT_Physics_v1")                                                                        ) HLT_ |= (bit<<52);
+    if      (!name.compare("HLT_Physics_v")                              ) HLT_ |= (bit<<  0);  // bit 0 (lowest)
+    else if (!name.compare("HLT_L1SingleMu3p5_WideWindow_v" )            ) HLT_ |= (bit<<  1);  // bit 1
+    else if (!name.compare("HLT_L1SingleMu3p5_v" )                       ) HLT_ |= (bit<<  2);  
+    else if (!name.compare("HLT_L1SingleMuOpen_WideWindow_v" )           ) HLT_ |= (bit<<  3);  
+    else if (!name.compare("HLT_L1SingleMuOpen_v" )                      ) HLT_ |= (bit<<  4);  
+    else if (!name.compare("HLT_L1SingleEG5_WideWindow_v")               ) HLT_ |= (bit<<  5); 
+    else if (!name.compare("HLT_L1SingleEG5_v")                          ) HLT_ |= (bit<<  6);  
+    else if (!name.compare("HLT_L1SingleEG20_WideWindow_v")              ) HLT_ |= (bit<<  7);  
+    else if (!name.compare("HLT_L1SingleEG20_v")                         ) HLT_ |= (bit<<  8);  
+    else if (!name.compare("HLT_L1SingleJet36_WideWindow_v")             ) HLT_ |= (bit<<  9);
+    else if (!name.compare("HLT_L1SingleJet36_v")                        ) HLT_ |= (bit<< 10);
+    else if (!name.compare("HLT_L1SingleJet68_WideWindow_v")             ) HLT_ |= (bit<< 11);
+    else if (!name.compare("HLT_L1SingleJet68_v")                        ) HLT_ |= (bit<< 12);
+    else if (!name.compare("HLT_ZeroBias_")                              ) HLT_ |= (bit<< 13);
   }
 
   edm::Handle<edm::View<pat::MET> > pfMETHandle;
