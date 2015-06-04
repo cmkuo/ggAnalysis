69c6ea9f5059dcd4da443c2b1c5327af353e6da9
Maxim <konush@cern.ch>
2015-06-04 12:32:35 +0200

    add genMET tree branches if doGenParticles is true

--- ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
+++ ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
@@ -35,8 +35,10 @@ void ggNtuplizer::branchesGlobalEvent(TTree* tree) {
   tree->Branch("HLT",     &HLT_);
   tree->Branch("HLTIsPrescaled", &HLTIsPrescaled_);
 
-  tree->Branch("genMET",      &genMET_);
-  tree->Branch("genMETPhi",   &genMETPhi_);
+  if (doGenParticles_) {
+    tree->Branch("genMET",      &genMET_);
+    tree->Branch("genMETPhi",   &genMETPhi_);
+  }
   tree->Branch("pfMET",       &pfMET_);
   tree->Branch("pfMETPhi",    &pfMETPhi_);
   tree->Branch("pfMETsumEt",  &pfMETsumEt_);
