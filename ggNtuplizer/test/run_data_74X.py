5f0fa843db4694993ab42252939f7c73fb7b6a22
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2015-07-14 16:12:00 +0200

    this is the py file for both AOD and miniAOD for running on data !

---
+++ ggNtuplizer/test/run_data_74X.py
@@ -0,0 +1,92 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ggKit')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load("Configuration.Geometry.GeometryIdeal_cff" )
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff" )
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+
+#process.Tracer = cms.Service("Tracer")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+        '/store/data/Run2015B/DoubleEG/AOD/PromptReco-v1/000/251/244/00000/FA8EC6E3-D528-E511-B1D5-02163E012BD2.root'
+        #'/store/data/Run2015B/DoubleEG/MINIAOD/PromptReco-v1/000/251/244/00000/6A0A8868-4B27-E511-B3F8-02163E011BD1.root'
+)
+                            )
+
+#process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
+process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
+
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+runOnData( process, outputModules = [] )
+#removeMCMatching(process, names=['All'], outputModules=[])
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_data.root'))
+
+useAOD = True
+#####VID framework####################
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format  to be
+# DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
+if useAOD == True :
+    dataFormat = DataFormat.AOD
+    process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_cfi")
+    from JMEAnalysis.JetToolbox.jetToolbox_cff import *
+    jetToolbox( process, 'ak4', 'ak4PFJetsCHS', 'out', miniAOD= False, addSoftDrop=True, addSoftDropSubjets=True, addNsub=True )
+    jetToolbox( process, 'ak8', 'ak8PFJetsCHS', 'out', miniAOD= False, addSoftDrop=True, addSoftDropSubjets=True, addNsub=True )
+else :
+    dataFormat = DataFormat.MiniAOD
+    process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
+
+process.ggNtuplizer.isAOD=cms.bool(useAOD)
+process.ggNtuplizer.doGenParticles=cms.bool(False)
+process.ggNtuplizer.dumpSubJets=cms.bool(True)
+process.ggNtuplizer.dumpJets=cms.bool(True)
+process.ggNtuplizer.dumpTaus=cms.bool(True)
+
+switchOnVIDElectronIdProducer(process, dataFormat)
+switchOnVIDPhotonIdProducer(process, dataFormat)
+
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V2_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_PHYS14_PU20bx25_nonTrig_V1_cff']
+
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+    
+my_phoid_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_PHYS14_PU20bx25_V2_cff',
+                    'RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_50ns_nonTrig_V0_cff']
+
+#add them to the VID producer
+for idmod in my_phoid_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
+
+process.p = cms.Path(
+    ###process.egmGsfElectronIDSequence
+    # process.mvaTrigV050nsCSA14
+    # + process.mvaTrigV025nsCSA14
+    # + process.mvaNonTrigV050nsCSA14
+    # + process.mvaNonTrigV025nsCSA14
+    # + process.mvaNonTrigV025nsPHYS14 
+#    process.patDefaultSequence *
+    process.egmGsfElectronIDSequence 
+    * process.egmPhotonIDSequence 
+    * process.ggNtuplizer
+    )
+
+
+#print process.dumpPython()
