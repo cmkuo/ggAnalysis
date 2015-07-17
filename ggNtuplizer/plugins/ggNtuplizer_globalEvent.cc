73ce1eedc6b6b02d52a9bb38cb2abcb9e0ff45f3
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2015-07-17 16:24:36 +0200

    change nVtx to the sum of all reconstructed vertices

--- ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
+++ ggNtuplizer/plugins/ggNtuplizer_globalEvent.cc
@@ -74,16 +74,16 @@ void ggNtuplizer::fillGlobalEvent(const edm::Event& e, const edm::EventSetup& es
     nVtx_ = 0;
     
     for (vector<reco::Vertex>::const_iterator v = vtxHandle->begin(); v != vtxHandle->end(); ++v) {
-      bool isFake = isAOD_ ? v->isFake() : (v->chi2() == 0 && v->ndof() == 0);
-      if (!isFake) {
-        if (nVtx_ == 0) {
-	  nTrksPV_ = v->nTracks();
-	  vtx_     = v->x();
-	  vty_     = v->y();
-	  vtz_     = v->z();
-	}
-        nVtx_++;
+      nVtx_++;
+      //bool isFake = isAOD_ ? v->isFake() : (v->chi2() == 0 && v->ndof() == 0);
+      //if (!isFake) {
+      if (nVtx_ == 0) {
+	nTrksPV_ = v->nTracks();
+	vtx_     = v->x();
+	vty_     = v->y();
+	vtz_     = v->z();
       }
+      //}
     }
   } else
     edm::LogWarning("ggNtuplizer") << "Primary vertices info not unavailable";
