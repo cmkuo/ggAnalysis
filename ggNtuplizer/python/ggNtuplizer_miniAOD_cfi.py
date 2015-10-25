77663fe4ca16f6715e5ca578c81621ec6b7b877f
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2015-10-25 11:45:29 +0100

    update MET filters and split MET from globalEvent.cc and turn off METnoHF

--- ggNtuplizer/python/ggNtuplizer_miniAOD_cfi.py
+++ ggNtuplizer/python/ggNtuplizer_miniAOD_cfi.py
@@ -20,7 +20,7 @@ ggNtuplizer = cms.EDAnalyzer("ggNtuplizer",
                              development      = cms.bool(False),
                              addFilterInfoAOD = cms.bool(False),
                              addFilterInfoMINIAOD = cms.bool(True),
-                             doNoHFMET            = cms.bool(True),
+                             doNoHFMET            = cms.bool(False),
 
                              trgFilterDeltaPtCut = cms.double(0.5),
                              trgFilterDeltaRCut  = cms.double(0.3),
