e4c3494cbdb9314280e04d5a0bb6d9b835223312
abdollah <abdollah.mohammadi@cern.ch>
2015-12-14 20:11:50 +0100

    adding GenHT

--- ggNtuplizer/plugins/ggNtuplizer_genParticles.cc
+++ ggNtuplizer/plugins/ggNtuplizer_genParticles.cc
@@ -2,6 +2,8 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "ggAnalysis/ggNtuplizer/interface/ggNtuplizer.h"
 #include "ggAnalysis/ggNtuplizer/interface/GenParticleParentage.h"
+// import LHEEventProduction definition
+#include "SimDataFormats/GeneratorProducts/interface/LHEEventProduct.h"
 
 using namespace std;
 
@@ -10,6 +12,7 @@ vector<float>    pdf_;
 float            pthat_;
 float            processID_;
 float            genWeight_;
+float            genHT_;
 
 Int_t            nPUInfo_;
 vector<int>      nPU_;
@@ -41,6 +44,7 @@ vector<float>    mcCalIsoDR03;
 vector<float>    mcTrkIsoDR03;
 vector<float>    mcCalIsoDR04;
 vector<float>    mcTrkIsoDR04;
+
 
 TH1*             hPU_;
 TH1*             hPUTrue_;
@@ -113,6 +117,7 @@ void ggNtuplizer::branchesGenInfo(TTree* tree, edm::Service<TFileService> &fs) {
   tree->Branch("pthat",        &pthat_);
   tree->Branch("processID",    &processID_);
   tree->Branch("genWeight",    &genWeight_);
+tree->Branch("genHT",    &genHT_);
 
   tree->Branch("nPUInfo",      &nPUInfo_);
   tree->Branch("nPU",          &nPU_);
@@ -158,6 +163,7 @@ void ggNtuplizer::fillGenInfo(const edm::Event& e) {
   pthat_     = -99;
   processID_ = -99;
   genWeight_ = -99;
+    genHT_=-99;
   pdf_.clear();
 
   nPUInfo_ = 0;
@@ -188,6 +194,25 @@ void ggNtuplizer::fillGenInfo(const edm::Event& e) {
   } else
     edm::LogWarning("ggNtuplizer") << "no GenEventInfoProduct in event";
 
+    
+    // access generator level HT
+    edm::Handle<LHEEventProduct> lheEventProduct;
+    e.getByLabel( "externalLHEProducer", lheEventProduct);
+    const lhef::HEPEUP& lheEvent = lheEventProduct->hepeup();
+    std::vector<lhef::HEPEUP::FiveVector> lheParticles = lheEvent.PUP;
+    double lheHt = 0.;
+    size_t numParticles = lheParticles.size();
+    for ( size_t idxParticle = 0; idxParticle < numParticles; ++idxParticle ) {
+        int absPdgId = TMath::Abs(lheEvent.IDUP[idxParticle]);
+        int status = lheEvent.ISTUP[idxParticle];
+        if ( status == 1 && ((absPdgId >= 1 && absPdgId <= 6) || absPdgId == 21) ) { // quarks and gluons
+            lheHt += TMath::Sqrt(TMath::Power(lheParticles[idxParticle][0], 2.) + TMath::Power(lheParticles[idxParticle][1], 2.)); // first entry is px, second py
+        } 
+    }
+    genHT_=lheHt;
+    
+    
+    
   edm::Handle<vector<PileupSummaryInfo> > genPileupHandle;
   e.getByToken(puCollection_, genPileupHandle);
 
