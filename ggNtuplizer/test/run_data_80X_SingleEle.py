40669d23e3f2b472494f116c97effefbb95c68cc
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2017-02-19 07:57:37 +0100

    update

--- ggNtuplizer/test/run_data_80X_SingleEle.py
+++ ggNtuplizer/test/run_data_80X_SingleEle.py
@@ -11,35 +11,36 @@ process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cf
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_2016SeptRepro_v4')
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_2016SeptRepro_v7')
 
 #process.Tracer = cms.Service("Tracer")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 #jec from sqlite
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 from CondCore.DBCommon.CondDBSetup_cfi import *
 process.jec = cms.ESSource("PoolDBESSource",CondDBSetup,
- connect = cms.string('sqlite:Spring16_25nsV10All_DATA.db'),
+ connect = cms.string('sqlite:Summer16_23Sep2016AllV4_DATA.db'),
  toGet = cms.VPSet(
  cms.PSet(
   record = cms.string('JetCorrectionsRecord'),
-  tag = cms.string('JetCorrectorParametersCollection_Spring16_25nsV10All_DATA_AK4PFchs'),
+  tag = cms.string('JetCorrectorParametersCollection_Summer16_23Sep2016AllV4_DATA_AK4PFchs'),
   label = cms.untracked.string('AK4PFchs')
  ),
  cms.PSet(
   record = cms.string('JetCorrectionsRecord'),
-  tag = cms.string('JetCorrectorParametersCollection_Spring16_25nsV10All_DATA_AK8PFchs'),
+  tag = cms.string('JetCorrectorParametersCollection_Summer16_23Sep2016AllV4_DATA_AK8PFchs'),
   label = cms.untracked.string('AK8PFchs')
   )))
 process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-        '/store/data/Run2016G/SingleElectron/MINIAOD/23Sep2016-v1/100000/02A99D82-C590-E611-9030-002590E7E02E.root'
-        #'/store/data/Run2016B/SingleElectron/MINIAOD/23Sep2016-v3/00000/00099863-E799-E611-A876-141877343E6D.root'
+        '/store/data/Run2016E/DoubleMuon/MINIAOD/03Feb2017-v1/100000/062FB971-1AED-E611-965F-0CC47A4C8F12.root'
+        #'/store/data/Run2016H/DoubleMuon/MINIAOD/PromptReco-v3/000/284/036/00000/04DC0281-C89F-E611-81C6-02163E0141E6.root'
+        #'/store/data/Run2016B/SingleElectron/MINIAOD/23Sep2016-v2/80000/5A4402F5-638C-E611-A471-0025905A60AA.root'
         )
                             )
 
@@ -49,22 +50,14 @@ process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
 process.load( "PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff" )
 process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
 
-#process.load('EgammaAnalysis.ElectronTools.calibratedElectronsRun2_cfi')
-#process.load('EgammaAnalysis.ElectronTools.calibratedPhotonsRun2_cfi')
-process.calibratedPatElectrons = cms.EDProducer("CalibratedPatElectronProducerRun2",
-    correctionFile = cms.string('EgammaAnalysis/ElectronTools/data/ScalesSmearings/Winter_2016_reReco_v1_ele'),
-    electrons = cms.InputTag("slimmedElectrons"),
-    gbrForestName = cms.string('gedelectron_p4combination_25ns'),
-    isMC = cms.bool(False),
-    isSynchronization = cms.bool(False)
-)
+### EGM 80X regression
+from EgammaAnalysis.ElectronTools.regressionWeights_cfi import regressionWeights
+process = regressionWeights(process)
+process.load('EgammaAnalysis.ElectronTools.regressionApplication_cff')
 
-process.calibratedPatPhotons = cms.EDProducer("CalibratedPatPhotonProducerRun2",
-    correctionFile = cms.string('EgammaAnalysis/ElectronTools/data/ScalesSmearings/Winter_2016_reReco_v1_ele'),
-    photons = cms.InputTag("slimmedPhotons"),
-    isMC = cms.bool(False),
-    isSynchronization = cms.bool(False)
-)
+### EGM scale and smearing correction
+process.load('EgammaAnalysis.ElectronTools.calibratedPatElectronsRun2_cfi')
+process.load('EgammaAnalysis.ElectronTools.calibratedPatPhotonsRun2_cfi')
 
 #from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
 from PhysicsTools.PatAlgos.tools.coreTools import *
@@ -84,9 +77,9 @@ runOnData( process,  names=['Photons', 'Electrons','Muons','Taus','Jets'], outpu
 process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_data.root'))
 
 jecLevels = [
-  'Spring16_25nsV10p2_DATA_L2Relative_AK8PFchs.txt',
-  'Spring16_25nsV10p2_DATA_L3Absolute_AK8PFchs.txt',
-  'Spring16_25nsV10p2_DATA_L2L3Residual_AK8PFchs.txt'
+  'Summer16_23Sep2016BCDV4_DATA_L2Relative_AK8PFchs.txt',
+  'Summer16_23Sep2016BCDV4_DATA_L3Absolute_AK8PFchs.txt',
+  'Summer16_23Sep2016BCDV4_DATA_L2L3Residual_AK8PFchs.txt'
 ]
 
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
@@ -100,7 +93,9 @@ updateJetCollection(
     process,
     jetSource = cms.InputTag('slimmedJetsAK8'),
     labelName = 'UpdatedJECAK8',
-    jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'None')
+    jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual']), 'None'),
+    btagDiscriminators = ['pfBoostedDoubleSecondaryVertexAK8BJetTags'],
+    btagPrefix = 'newV4' # optional, in case interested in accessing both the old and new discriminator values
     )
 
 # MET correction and uncertainties
@@ -120,6 +115,8 @@ process.ggNtuplizer.doGenParticles=cms.bool(False)
 process.ggNtuplizer.dumpSubJets=cms.bool(True)
 process.ggNtuplizer.dumpJets=cms.bool(True)
 process.ggNtuplizer.dumpTaus=cms.bool(False)
+## the following line is only needed when you run on Feb 2017 re-miniAOD
+process.ggNtuplizer.patTriggerResults=cms.InputTag("TriggerResults", "", "PAT")
 
 #####VID framework####################
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
@@ -137,29 +134,36 @@ my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElect
 #add them to the VID producer
 for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-        
+                                                                
 my_phoid_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Spring16_V2p2_cff',
-                    'RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring16_nonTrig_V1_cff']                       
+                    'RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring16_nonTrig_V1_cff']
+
+process.load("RecoEgamma.ElectronIdentification.ElectronIDValueMapProducer_cfi")
+process.electronIDValueMapProducer.srcMiniAOD = cms.InputTag('slimmedElectrons')
+process.electronMVAValueMapProducer.srcMiniAOD = cms.InputTag('slimmedElectrons')
+process.photonIDValueMapProducer.srcMiniAOD = cms.InputTag('slimmedPhotons')
+process.photonMVAValueMapProducer.srcMiniAOD = cms.InputTag('slimmedPhotons')
 
 #add them to the VID producer
 for idmod in my_phoid_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
-
+    
 process.singleEleHLTFilter = cms.EDFilter("HLTHighLevel",
                                           eventSetupPathsKey = cms.string(''),
                                           TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
                                           HLTPaths = cms.vstring('HLT_Ele27_WPLoose_Gsf_v*', 'HLT_Ele27_WPTight_Gsf_v*', 'HLT_Ele27_eta2p1_WPLoose_Gsf_v*', 'HLT_Ele27_eta2p1_WPTight_Gsf_v*', 'HLT_Ele32_eta2p1_WPTight_Gsf_v*', 'HLT_Ele35_WPLoose_Gsf_v*', 'HLT_Ele45_WPLoose_Gsf_v*'),
-                                          andOr = cms.bool(True), 
-                                          throw = cms.bool(False)
+                                          andOr = cms.bool(True), # True = OR, False = AND
+                                          throw = cms.bool(True) # Tolerate if triggers not available
                                           )
 
 process.p = cms.Path(
     ###process.reapplyJEC*
     ###process.pfImpactParameterTagInfosAK8 *
     ###process.pfInclusiveSecondaryVertexFinderTagInfosAK8 *
-    ###process.pfBoostedDoubleSecondaryVertexAK8BJetTags *
+    ###process.pfBoostedDoubleSecondaryVertexAK8BJetTags *        
     process.singleEleHLTFilter*
     process.ggMETFiltersSequence* 
+    process.regressionApplication*
     process.calibratedPatElectrons*
     process.calibratedPatPhotons*
     process.egmGsfElectronIDSequence*
