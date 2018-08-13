fd12e64f79050d5662e08503def352f52d268523
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2018-08-13 14:57:21 +0200

    update py cfg for 2017 MC

--- ggNtuplizer/test/run_mc_94X.py
+++ ggNtuplizer/test/run_mc_94X.py
@@ -7,11 +7,11 @@ process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 #process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
 process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v12')
-process.load("Configuration.StandardSequences.MagneticField_cff")
 
 #process.Tracer = cms.Service("Tracer")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
@@ -19,8 +19,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-        #'file:/data4/cmkuo/testfiles/DYJetsToLL_M-50_RunIIFall17.root'        
-        '/store/mc/RunIIFall17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/00000/005DC030-D3F4-E711-889A-02163E01A62D.root'
+        'file:/data4/cmkuo/testfiles/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_RunIIFall17MiniAODv2.root'
         ))
 
 #process.load("PhysicsTools.PatAlgos.patSequences_cff")
@@ -28,28 +27,11 @@ process.source = cms.Source("PoolSource",
 process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
 process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
 
-### EGM scale and smearing correction         
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-                                                  calibratedPatElectrons = cms.PSet(
-    initialSeed = cms.untracked.uint32(12345),
-    engineName = cms.untracked.string('TRandom3')
-    ),
-                                                  calibratedPatPhotons = cms.PSet(
-    initialSeed = cms.untracked.uint32(12345),
-    engineName = cms.untracked.string('TRandom3')
-    ),
-                                                   ggNtuplizer  = cms.PSet(
-    initialSeed = cms.untracked.uint32(67890),
-    engineName = cms.untracked.string('TRandom3')
-    )
-                                                   )
-
-process.load('EgammaAnalysis.ElectronTools.calibratedPatElectronsRun2_cfi')
-process.load('EgammaAnalysis.ElectronTools.calibratedPatPhotonsRun2_cfi')
-process.calibratedPatElectrons.isMC = cms.bool(True)
-process.calibratedPatPhotons.isMC = cms.bool(True)
-
-process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+### fix a bug in the ECAL-Tracker momentum combination when applying the scale and smearing
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+setupEgammaPostRecoSeq(process,
+                       runVID=False, 
+                       era='2017-Nov17ReReco')  
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_mc.root'))
 
@@ -60,50 +42,17 @@ runMetCorAndUncFromMiniAOD(process,
                            )
 
 process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
-process.ggNtuplizer.dumpSoftDrop= cms.bool(True)
-process.ggNtuplizer.runHFElectrons=cms.bool(True)
-process.ggNtuplizer.isAOD=cms.bool(False)
+process.ggNtuplizer.year=cms.int32(2017)
 process.ggNtuplizer.doGenParticles=cms.bool(True)
-process.ggNtuplizer.dumpSubJets=cms.bool(False)
+process.ggNtuplizer.dumpPFPhotons=cms.bool(False)
+process.ggNtuplizer.dumpHFElectrons=cms.bool(False)
 process.ggNtuplizer.dumpJets=cms.bool(True)
+process.ggNtuplizer.dumpAK8Jets=cms.bool(False)
+process.ggNtuplizer.dumpSoftDrop= cms.bool(True)
 process.ggNtuplizer.dumpTaus=cms.bool(False)
 process.ggNtuplizer.triggerEvent=cms.InputTag("slimmedPatTrigger", "", "PAT")
 
-#####VID framework####################
-from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
-dataFormat = DataFormat.MiniAOD
-switchOnVIDElectronIdProducer(process, dataFormat)
-switchOnVIDPhotonIdProducer(process, dataFormat)
-
-# define which IDs we want to produce
-my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Fall17_94X_V1_cff',
-                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV70_cff',
-                 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V1_cff',
-                 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V1_cff']
-
-#add them to the VID producer
-for idmod in my_id_modules:
-    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-    
-my_phoid_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Fall17_94X_V1_TrueVtx_cff',
-                    'RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Fall17_94X_V1_cff']
-
-process.load("RecoEgamma.ElectronIdentification.ElectronIDValueMapProducer_cfi")
-process.electronIDValueMapProducer.srcMiniAOD = cms.InputTag('slimmedElectrons')
-process.electronMVAValueMapProducer.srcMiniAOD = cms.InputTag('slimmedElectrons')
-process.photonIDValueMapProducer.srcMiniAOD = cms.InputTag('slimmedPhotons')
-process.photonMVAValueMapProducer.srcMiniAOD = cms.InputTag('slimmedPhotons')
-
-#add them to the VID producer
-for idmod in my_phoid_modules:
-    setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
-
 process.p = cms.Path(
-#    process.regressionApplication*
-    process.calibratedPatElectrons*
-    process.calibratedPatPhotons* 
-    process.egmGsfElectronIDSequence*
-    process.egmPhotonIDSequence*
     process.ggNtuplizer
     )
 
