18b92cf1e03ff02b9a6dc78eb514218ae6de094d
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2018-08-13 23:29:43 +0200

    add new py cfg files and remove the old ones

---
+++ ggNtuplizer/test/run_data2016_94X.py
@@ -0,0 +1,68 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ggKit')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load("Configuration.Geometry.GeometryIdeal_cff" )
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff" )
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_ReReco_EOY17_v6')
+
+#process.Tracer = cms.Service("Tracer")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+        'file:/data4/cmkuo/testfiles/DoubleEG_Run2016F_17Jul2018.root'
+        )
+                            )
+
+#process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
+process.load( "PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff" )
+process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
+
+### fix a bug in the ECAL-Tracker momentum combination when applying the scale and smearing
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+setupEgammaPostRecoSeq(process,
+                       runVID=False,
+                       era='2016-Legacy') 
+
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+runOnData( process,  names=['Photons', 'Electrons','Muons','Taus','Jets'], outputModules = [] )
+#runOnData( process, outputModules = [] )
+#removeMCMatching(process, names=['All'], outputModules=[])
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_data.root'))
+
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD (
+        process,
+        isData = True
+)
+
+process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
+process.ggNtuplizer.year=cms.int32(2016)
+process.ggNtuplizer.doGenParticles=cms.bool(False)
+process.ggNtuplizer.dumpPFPhotons=cms.bool(False)
+process.ggNtuplizer.dumpHFElectrons=cms.bool(False)
+process.ggNtuplizer.dumpJets=cms.bool(True)
+process.ggNtuplizer.dumpAK8Jets=cms.bool(False)
+process.ggNtuplizer.dumpSoftDrop= cms.bool(True)
+process.ggNtuplizer.dumpTaus=cms.bool(False)
+process.ggNtuplizer.patTriggerResults=cms.InputTag("TriggerResults", "", "DQM")
+
+process.p = cms.Path(
+    process.ggNtuplizer
+    )
+
+#print process.dumpPython()
---
+++ ggNtuplizer/test/run_mc2016_94X.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ggKit')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v12')
+
+#process.Tracer = cms.Service("Tracer")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+        'file:/data4/cmkuo/testfiles/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer16MiniAODv2.root'
+        ))
+
+#process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
+process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
+
+### fix a bug in the ECAL-Tracker momentum combination when applying the scale and smearing
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+setupEgammaPostRecoSeq(process,
+                       runVID=False, 
+                       era='2016-Legacy')  
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_mc.root'))
+
+# MET correction and uncertainties
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD(process,
+                           isData=False
+                           )
+
+process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
+process.ggNtuplizer.year=cms.int32(2016)
+process.ggNtuplizer.doGenParticles=cms.bool(True)
+process.ggNtuplizer.dumpPFPhotons=cms.bool(False)
+process.ggNtuplizer.dumpHFElectrons=cms.bool(False)
+process.ggNtuplizer.dumpJets=cms.bool(True)
+process.ggNtuplizer.dumpAK8Jets=cms.bool(False)
+process.ggNtuplizer.dumpSoftDrop= cms.bool(True)
+process.ggNtuplizer.dumpTaus=cms.bool(False)
+process.ggNtuplizer.patTriggerResults=cms.InputTag("TriggerResults", "", "PAT")
+process.ggNtuplizer.triggerEvent=cms.InputTag("selectedPatTrigger", "", "")
+
+process.p = cms.Path(
+    process.ggNtuplizer
+    )
+
+#print process.dumpPython()
