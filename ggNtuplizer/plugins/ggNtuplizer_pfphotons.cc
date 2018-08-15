66b834e583872afda5ea04ebe78d974ef5ba23bd
Chia Ming, Kuo <chia-ming.kuo@cern.ch>
2018-08-15 14:57:07 +0200

    add two variables for e/g SC veto

--- ggNtuplizer/plugins/ggNtuplizer_pfphotons.cc
+++ ggNtuplizer/plugins/ggNtuplizer_pfphotons.cc
@@ -13,30 +13,42 @@ vector<float>  pfPhoPhi_;
 vector<float>  pfPhoChIso_;
 vector<float>  pfPhoPhoIso_;
 vector<float>  pfPhoNeuIso_;
+vector<int>    pfPhoEleIndex_;
+vector<int>    pfPhoPhoIndex_;
 
 void ggNtuplizer::branchesPFPhotons(TTree* tree) {
   
-  tree->Branch("npfPho",      &npfPho_);
-  tree->Branch("pfPhoEt",     &pfPhoEt_);
-  tree->Branch("pfPhoEta",    &pfPhoEta_);
-  tree->Branch("pfPhoPhi",    &pfPhoPhi_);
-  tree->Branch("pfPhoChIso",  &pfPhoChIso_);
-  tree->Branch("pfPhoPhoIso", &pfPhoPhoIso_);
-  tree->Branch("pfPhoNeuIso", &pfPhoNeuIso_);
+  tree->Branch("npfPho",        &npfPho_);
+  tree->Branch("pfPhoEt",       &pfPhoEt_);
+  tree->Branch("pfPhoEta",      &pfPhoEta_);
+  tree->Branch("pfPhoPhi",      &pfPhoPhi_);
+  tree->Branch("pfPhoChIso",    &pfPhoChIso_);
+  tree->Branch("pfPhoPhoIso",   &pfPhoPhoIso_);
+  tree->Branch("pfPhoNeuIso",   &pfPhoNeuIso_);
+  tree->Branch("pfPhoEleIndex", &pfPhoEleIndex_);
+  tree->Branch("pfPhoPhoIndex", &pfPhoPhoIndex_);
 }
 
 void ggNtuplizer::fillPFPhotons(const edm::Event& e, const edm::EventSetup& es) {
   
   // cleanup from previous execution
-  pfPhoEt_    .clear();
-  pfPhoEta_   .clear();
-  pfPhoPhi_   .clear();
-  pfPhoChIso_ .clear();
-  pfPhoPhoIso_.clear();
-  pfPhoNeuIso_.clear();
+  pfPhoEt_      .clear();
+  pfPhoEta_     .clear();
+  pfPhoPhi_     .clear();
+  pfPhoChIso_   .clear();
+  pfPhoPhoIso_  .clear();
+  pfPhoNeuIso_  .clear();
+  pfPhoEleIndex_.clear();
+  pfPhoPhoIndex_.clear();
 
   npfPho_ = 0;
 
+  edm::Handle<edm::View<pat::Electron> > electronHandle;
+  e.getByToken(electronCollection_, electronHandle);
+
+  edm::Handle<edm::View<pat::Photon> > photonHandle;
+  e.getByToken(photonCollection_, photonHandle);
+  
   edm::Handle<pat::PackedCandidateCollection> cands;
   e.getByToken(pckPFCandidateCollection_, cands);
 
@@ -44,6 +56,24 @@ void ggNtuplizer::fillPFPhotons(const edm::Event& e, const edm::EventSetup& es) 
     const pat::PackedCandidate &pf = (*cands)[i];
 
     if (pf.pdgId() == 22 && pf.pt() > 1.5 && fabs(pf.eta()) < 2.5) {
+
+      int nEle_     = 0;
+      int eleIndex_ = -1;
+      for (edm::View<pat::Electron>::const_iterator iEle = electronHandle->begin(); iEle != electronHandle->end(); ++iEle) {
+	for (const edm::Ref<pat::PackedCandidateCollection> & refEle : iEle->associatedPackedPFCandidates()) {
+	  if (reco::deltaR(pf.p4(), refEle->p4()) < 0.0001) eleIndex_ = nEle_;
+	}
+	nEle_++;
+      }
+      
+      int nPho_     = 0;
+      int phoIndex_ = -1;
+      for (edm::View<pat::Photon>::const_iterator iPho = photonHandle->begin(); iPho != photonHandle->end(); ++iPho) {
+	for (const edm::Ref<pat::PackedCandidateCollection> & refPho : iPho->associatedPackedPFCandidates()) {
+	  if (reco::deltaR(pf.p4(), refPho->p4()) < 0.0001) phoIndex_ = nPho_;
+	}
+	nPho_++;
+      }
 
       float chiso_  = 0;
       float phoiso_ = 0;
@@ -70,12 +100,14 @@ void ggNtuplizer::fillPFPhotons(const edm::Event& e, const edm::EventSetup& es) 
 
       }
 
-      pfPhoEt_    .push_back(pf.pt());
-      pfPhoEta_   .push_back(pf.eta());
-      pfPhoPhi_   .push_back(pf.phi());
-      pfPhoChIso_ .push_back(chiso_);
-      pfPhoPhoIso_.push_back(phoiso_);
-      pfPhoNeuIso_.push_back(neuiso_);
+      pfPhoEt_      .push_back(pf.pt());
+      pfPhoEta_     .push_back(pf.eta());
+      pfPhoPhi_     .push_back(pf.phi());
+      pfPhoChIso_   .push_back(chiso_);
+      pfPhoPhoIso_  .push_back(phoiso_);
+      pfPhoNeuIso_  .push_back(neuiso_);
+      pfPhoEleIndex_.push_back(eleIndex_);
+      pfPhoPhoIndex_.push_back(phoIndex_);
 
       npfPho_++;
     }
