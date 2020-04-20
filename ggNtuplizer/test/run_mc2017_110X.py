d403285bd0efab27efee5993aa8195ce450fb294
cmkuo <chia-ming.kuo@cern.ch>
2020-04-20 15:09:30 +0200

    new file for UL2017

---
+++ ggNtuplizer/test/run_mc2017_110X.py
@@ -0,0 +1,102 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ggKit')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v18')
+
+#process.Tracer = cms.Service("Tracer")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(
+                                '/store/mc/RunIISummer19UL17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/106X_mc2017_realistic_v6-v2/280000/FA2E91EA-C126-BF43-BCFA-54E3AB1D7CEA.root'
+                            ))
+
+#process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+process.load( "PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff" )
+process.load( "PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff" )
+
+### fix a bug in the ECAL-Tracker momentum combination when applying the scale and smearing
+from RecoEgamma.EgammaTools.EgammaPostRecoTools import setupEgammaPostRecoSeq
+setupEgammaPostRecoSeq(process,
+                       runVID=False, 
+                       era='2017-Nov17ReReco',
+                       eleIDModules=['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Fall17_94X_V2_cff',
+                                     'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV70_cff',
+                                     'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_iso_V2_cff',
+                                     'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Fall17_noIso_V2_cff'],
+                       phoIDModules=['RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Fall17_94X_V2_cff',
+                                     'RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_Fall17_94X_V2_cff']
+                       )
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('ggtree_mc.root'))
+
+### update JEC
+process.load("PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff")
+process.jetCorrFactors = process.updatedPatJetCorrFactors.clone(
+    src = cms.InputTag("slimmedJets"),
+    levels = ['L1FastJet', 'L2Relative', 'L3Absolute'],
+    payload = 'AK4PFchs') 
+
+process.slimmedJetsJEC = process.updatedPatJets.clone(
+    jetSource = cms.InputTag("slimmedJets"),
+    jetCorrFactorsSource = cms.VInputTag(cms.InputTag("jetCorrFactors"))
+    )
+
+### reduce effect of high eta EE noise on the PF MET measurement
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD (
+        process,
+        isData = False, # false for MC
+        fixEE2017 = True,
+        fixEE2017Params = {'userawPt': True, 'ptThreshold':50.0, 'minEtaThreshold':2.65, 'maxEtaThreshold': 3.139} ,
+        postfix = "ModifiedMET"
+)
+
+# random generator for jet smearing
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+                                                   ggNtuplizer  = cms.PSet(
+        initialSeed = cms.untracked.uint32(201678),
+        engineName = cms.untracked.string('TRandom3')
+        )
+                                                   )
+
+process.load("ggAnalysis.ggNtuplizer.ggNtuplizer_miniAOD_cfi")
+process.ggNtuplizer.year=cms.int32(2017)
+process.ggNtuplizer.doGenParticles=cms.bool(True)
+process.ggNtuplizer.dumpPFPhotons=cms.bool(True)
+process.ggNtuplizer.dumpHFElectrons=cms.bool(False)
+process.ggNtuplizer.dumpJets=cms.bool(True)
+process.ggNtuplizer.dumpAK8Jets=cms.bool(False)
+process.ggNtuplizer.dumpSoftDrop= cms.bool(True)
+process.ggNtuplizer.dumpTaus=cms.bool(False)
+process.ggNtuplizer.triggerEvent=cms.InputTag("slimmedPatTrigger", "", "PAT")
+process.ggNtuplizer.ak4JetSrc=cms.InputTag("slimmedJetsJEC")
+process.ggNtuplizer.pfMETLabel=cms.InputTag("slimmedMETsModifiedMET")
+
+process.cleanedMu = cms.EDProducer("PATMuonCleanerBySegments",
+                                   src = cms.InputTag("slimmedMuons"),
+                                   preselection = cms.string("track.isNonnull"),
+                                   passthrough = cms.string("isGlobalMuon && numberOfMatches >= 2"),
+                                   fractionOfSharedSegments = cms.double(0.499))
+
+process.p = cms.Path(
+    process.fullPatMetSequenceModifiedMET *
+    process.egammaPostRecoSeq *
+    process.cleanedMu *
+    process.jetCorrFactors *
+    process.slimmedJetsJEC *
+    process.ggNtuplizer
+    )
+
+#print process.dumpPython()
